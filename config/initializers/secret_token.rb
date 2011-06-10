0e7c818061b093ee6f1d38254f21090f7686770f
BRAINGAIN <braingain@braingain-System-Product-Name.(none)>
2011-06-10 15:24:00 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MalCom::Application.config.secret_token = '25feeaab5b4ee8be44890c8a0a41aa57d26b6edfcfabf8b2c8f3464a5275c1c9cf9b62f17b6ec93d2477cf598ac4cdc5c2ec48d375f1471adc26f172e3988aba'
