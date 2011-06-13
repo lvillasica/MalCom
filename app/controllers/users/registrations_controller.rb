f42365ec3594cffeeda59e39b3271945b24f59fe
Stefany Marie <stefanyserino@gmail.com>
2011-06-13 18:12:02 +0800

    can now update user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,10 +14,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   
   def update
+    @resource = User.find_by_id(params[:user][:id])
+    if @resource.update_with_password(params[resource_name])
+      redirect_to users_url
+    else
+      clean_up_passwords(@resource)
+      respond_with_navigational(resource){ render_with_scope :edit }
+    end
   end
-  
+
   def check_permissions
     authorize! :update, resource
   end
+  
 end
 
