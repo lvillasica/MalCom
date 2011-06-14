9bad337ba854d1bb73ea1c92495b089356cfc0f7
BRAINGAIN <braingain@braingain-System-Product-Name.(none)>
2011-06-14 16:51:16 +0800

    tickets update...

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,13 @@
 class Ticket < ActiveRecord::Base
 
   belongs_to :project
+  belongs_to :ticket
   #has_many :comments
   #has_many_and_belongs_to :tags
+  before_save :get_assigned
+  
+  def get_assigned
+    assigned = User.find(self.assigned_to).email
+  end
 
 end
