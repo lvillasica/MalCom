934db45fbd104ae767df95eb154631601cd04bbe
Stefany Serino <stefanyserino@gmail.com>
2011-06-18 01:19:05 +0800

    fixed tags (lil bit?) #afterpresentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-User.create(:email => 'admin@admin.com', :password => 'password', :password_confirmation => 'password', :admin => true, :status => 'Active').confirm!
-Role.create(:position => 'default',
+admin = User.create(:email => 'admin@admin.com', :password => 'password', :password_confirmation => 'password', :admin => true, :status => 'Active')
+admin.confirm!
+#admin.readonly!
+
+default_role = Role.create(:position => 'default',
             :can_create_project => false, :can_read_project => false, :can_update_project => false, :can_delete_project => false,
             :can_create_ticket => false, :can_read_ticket => false, :can_update_ticket => false, :can_delete_ticket => false,
             :can_create_comment => false, :can_read_comment => false, :can_delete_comment => false,
             :can_create_tag => false, :can_read_tag => false, :can_update_tag => false, :can_delete_tag => false,
-            :can_add_member => false, :can_remove_member => false, :can_assign_ticket => false).readonly!
-
-
+            :can_add_member => false, :can_remove_member => false, :can_assign_ticket => false)
+#default_role.readonly!
